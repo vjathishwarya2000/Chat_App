2fd3779a5c7261c5a102231d584d3f0dd9bc0c40
vjathishwarya2000 <vsjathu2000@gmail.com>
2025-03-02 15:09:54 +0530

    index file modified

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(cors({
 }));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:", PORT);
